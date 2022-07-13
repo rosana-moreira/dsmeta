098ddb9837880a7ed59086d44f9f74380b03ec5b
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-07-13 16:10:53 -0300

    Date select

--- server/src/main/java/com/dev/dsmeta/services/SaleService.java
+++ server/src/main/java/com/dev/dsmeta/services/SaleService.java
@@ -1,8 +1,11 @@
 package com.dev.dsmeta.services;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.dev.dsmeta.entities.Sale;
@@ -14,8 +17,10 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		LocalDate min = LocalDate.parse(minDate);
+		LocalDate max = LocalDate.parse(maxDate);
+		return repository.findSales(min, max,pageable);
 	}
 
 }

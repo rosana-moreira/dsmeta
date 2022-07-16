2b91ec3b55860ea6b95d685b07a3357afee7cbcc
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-07-16 16:17:00 -0300

    notification

--- web/src/components/NotificationButton/index.tsx
+++ web/src/components/NotificationButton/index.tsx
@@ -1,9 +1,19 @@
+import axios from "axios";
 import icon from "../../assets/img/notification-icon.svg";
-
+import { BASE_URL } from "../../utils/request";
 import "./styles.css";
-export function NotificationButton() {
+type Props = {
+  saleId: number;
+};
+export function NotificationButton({ saleId }: Props) {
+  function handleClick(id: number) {
+    axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+      console.log("Sussa");
+    });
+  }
+
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Notificar" />
     </div>
   );

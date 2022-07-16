d7c7dede11547bef1dcea8a6dbdbecaa5dbcaee5
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-07-16 16:22:21 -0300

    Toast

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,16 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Header } from "./components/Header";
-import { NotificationButton } from "./components/NotificationButton";
 import { SalesCard } from "./components/SalesCard";
 
 const App = () => (
   <>
+    <ToastContainer />
     <Header />
     <main>
       <section id="sales">
         <div className="dsmeta-container"></div>
-        <SalesCard/>
+        <SalesCard />
       </section>
     </main>
   </>
--- web/src/components/NotificationButton/index.tsx
+++ web/src/components/NotificationButton/index.tsx
@@ -1,14 +1,17 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import icon from "../../assets/img/notification-icon.svg";
 import { BASE_URL } from "../../utils/request";
 import "./styles.css";
+
 type Props = {
   saleId: number;
 };
+
 export function NotificationButton({ saleId }: Props) {
   function handleClick(id: number) {
     axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
-      console.log("Sussa");
+      toast.info("SMS Enviado com sucesso!");
     });
   }
 
